0fde3a2dbfed4dd82279d538bf72173ae84e55f0
Jay Nakrani <dhananjaynakrani@gmail.com>
2023-11-28 13:17:33 -0500

    Add set-up script to automate some of the installation steps.

--- app/api/instance-config/route.ts
+++ app/api/instance-config/route.ts
@@ -2,9 +2,13 @@ import { NextResponse } from "next/server";
 
 import { InstanceConfigResponse } from "lib/types/api/instance-config.response";
 
+// Stops pre-render at build time! NextJS framework assumes that build time env vars are
+// the same as run time env vars, and so it eagerly pre-renders this route!
+export const dynamic = 'force-dynamic';
 export async function GET() {
   const response: InstanceConfigResponse = {
     analytics: process.env.INSTANCE_CONFIG_ANALYTICS ?? "enabled",
+    instanceId: process.env.INSTANCE_CONFIG_INSTANCE_ID,
   };
   return NextResponse.json(response);
 }
---
+++ deployment/set-up.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env sh
+
+set -eu
+
+available() { command -v $1 >/dev/null; }
+require() {
+  local MISSING_TOOLS=''
+  for TOOL in $*; do
+    if ! available $TOOL; then
+      MISSING_TOOLS="$MISSING_TOOLS $TOOL"
+    fi
+  done
+
+  echo $MISSING_TOOLS
+}
+
+NEEDS=$(require curl openssl sed)
+if [ -n "$NEEDS" ]; then
+  echo "ERROR: Following tools are required but missing:"
+  for NEED in $NEEDS; do
+    echo "  - $NEED"
+  done
+  exit 1
+fi
+
+echo "Setting up instance"
+
+# Download docker-compose.yml file
+echo "  Downloading docker-compose.yml"
+curl -Os https://github.com/SecureAI-Tools/SecureAI-Tools/releases/latest/download/docker-compose.yml
+
+# Download .env file
+echo "  Generating .env file with sensible defaults"
+curl -s -o .env https://github.com/SecureAI-Tools/SecureAI-Tools/releases/latest/download/.env.example
+
+# Set NEXTAUTH_SECRET
+NEXTAUTH_SECRET=$(openssl rand -hex 32)
+sed -i '' "s/NEXTAUTH_SECRET=.*/NEXTAUTH_SECRET=\"${NEXTAUTH_SECRET}\"/g" .env
+
+# Set INSTANCE_CONFIG_INSTANCE_ID
+INSTANCE_CONFIG_INSTANCE_ID=$(openssl rand -hex 32)
+sed -i '' "s/INSTANCE_CONFIG_INSTANCE_ID=.*/INSTANCE_CONFIG_INSTANCE_ID=\"${INSTANCE_CONFIG_INSTANCE_ID}\"/g" .env
+
+
+echo "Instance has been configured 🎉"
+echo "  Run \"docker compose up -d\" to start the instance"
