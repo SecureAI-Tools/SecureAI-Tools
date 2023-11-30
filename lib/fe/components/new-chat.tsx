f7b63f5516b68e9c8e8e07217d1ffae5d9524e12
Jay Nakrani <dhananjaynakrani@gmail.com>
2023-11-29 19:26:28 -0500

    Improve new-chat page.

--- lib/fe/components/new-chat.tsx
+++ lib/fe/components/new-chat.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "flowbite-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { tw } from "twind";
+import Image from "next/image";
 
 import { post } from "lib/fe/api";
 import { organizationsIdOrSlugChatApiPath } from "lib/fe/api-paths";
@@ -57,9 +58,23 @@ export default function NewChat({ orgSlug }: { orgSlug: string }) {
     <>
       <StudioToasts toasts={toasts} />
       <div className={tw("flex flex-col w-full h-screen")}>
+        <div className={tw("flex flex-col grow items-center justify-center")}>
+          <div className={tw("flex flex-col items-center")}>
+            <Image
+              className={tw(`h-20 w-20`)}
+              src="/logo.png"
+              alt="logo"
+              width={80}
+              height={80}
+            />
+            <span className={tw("font-semibold text-xl mt-4")}>
+              How can I help you today?
+            </span>
+          </div>
+        </div>
         <div
           className={tw(
-            "absolute shrink-0 bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2 md:pl-2 md:w-[calc(100%-.5rem)]",
+            "shrink-0 bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2 md:pl-2 md:w-[calc(100%-.5rem)]",
           )}
         >
           <form
