2e10739c5138d7010d401ee65299aa3679391eb7
Jay Nakrani <dhananjaynakrani@gmail.com>
2023-12-04 20:08:03 -0500

    Improve PDF rendering.
    react-pdf-viewer has much smoother UX.

--- lib/fe/components/chat-with-docs.tsx
+++ lib/fe/components/chat-with-docs.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Button, Dropdown, Spinner } from "flowbite-react";
+import { Dropdown, Spinner } from "flowbite-react";
 import { tw } from "twind";
 import useSWR from "swr";
-import { Document as PDFDocument, Page as PDFPage, pdfjs } from "react-pdf";
-import "react-pdf/dist/Page/TextLayer.css";
 import { HiArrowTopRightOnSquare } from "react-icons/hi2";
 import Link from "next/link";
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 import { ChatResponse } from "lib/types/api/chat.response";
 import { Id } from "lib/types/core/id";
@@ -26,11 +27,9 @@ export function ChatWithDocs({
   chat: ChatResponse;
   chatMessages: ChatMessageResponse[];
 }) {
-  const [numPages, setNumPages] = useState<number | null>(null);
   const [previewDocumentId, setPreviewDocumentId] = useState<
     string | undefined
   >();
-  const [pageNumber, setPageNumber] = useState(1);
 
   const chatId = Id.from<ChatResponse>(chat.id);
   const documentCollectionId = Id.from<DocumentCollectionResponse>(chat.documentCollectionId!);
@@ -50,13 +49,6 @@ export function ChatWithDocs({
   );
 
   useEffect(() => {
-    pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-      "pdfjs-dist/build/pdf.worker.min.js",
-      import.meta.url,
-    ).toString();
-  }, []);
-
-  useEffect(() => {
     const responses = collectionDocumentsResponse?.response;
     if (responses && responses?.length > 0) {
       setPreviewDocumentId(responses[0].id);
@@ -67,26 +59,8 @@ export function ChatWithDocs({
     return renderErrors(fetchCollectionDocumentsError);
   }
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
-
-  function changePage(delta: number) {
-    setPageNumber((prevPageNumber) => prevPageNumber + delta);
-  }
-
   function changeDocument(docId: string) {
     setPreviewDocumentId(docId);
-    changePage(1);
-  }
-
-  function previousPage() {
-    changePage(-1);
-  }
-
-  function nextPage() {
-    changePage(1);
   }
 
   // Loading spinner for various doc preview elements
@@ -110,93 +84,64 @@ export function ChatWithDocs({
       ) : (
         <div className={tw("flex")}>
           <div className={tw("w-1/2 border-r-2")}>
-            <div>
-              <div>
-                <PDFDocument
-                  file={documentCollectionDocumentApiPath(
-                    documentCollectionId,
-                    Id.from(previewDocument.id),
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+              <div className={tw("flex flex-col h-screen")}>
+                <div
+                  className={tw(
+                    "flex flex-row flex p-4 bg-gray-50 top-0 items-center z-50 border-b border-black/10",
                   )}
-                  onLoadSuccess={onDocumentLoadSuccess}
-                  className={tw("max-h-screen overflow-scroll")}
-                  loading={renderLoadingSpinner}
                 >
-                  <div
-                    className={tw(
-                      "flex flex-row p-4 bg-gray-50 sticky top-0 items-center z-50 border-b border-black/10",
-                    )}
-                  >
-                    <div className={tw("grow flex flex-row justify-center")}>
-                      <Dropdown
-                        label={
-                          <span className={tw("font-semibold")}>
-                            {previewDocument.name}
-                          </span>
-                        }
-                        className={tw("z-50")}
-                        placement="bottom"
-                        inline
-                      >
-                        {collectionDocumentsResponse.response.map((doc) => (
-                          <Dropdown.Item
-                            onClick={() => {
-                              changeDocument(doc.id);
-                            }}
-                            key={doc.id}
-                          >
-                            <span
-                              className={tw(
-                                doc.id === previewDocumentId ? "font-bold" : "",
-                              )}
-                            >
-                              {doc.name}
-                            </span>
-                          </Dropdown.Item>
-                        ))}
-                      </Dropdown>
-                    </div>
-                    <Link
-                      href={documentCollectionDocumentApiPath(
-                        documentCollectionId,
-                        Id.from(previewDocument.id),
-                      )}
-                      target="_blank"
-                      className={tw("ml-3")}
-                      aria-label="open document in new tab"
+                  <div className={tw("grow flex flex-row justify-center")}>
+                    <Dropdown
+                      label={
+                        <span className={tw("font-semibold")}>
+                          {previewDocument.name}
+                        </span>
+                      }
+                      className={tw("z-50")}
+                      placement="bottom"
+                      inline
                     >
-                      <HiArrowTopRightOnSquare />
-                    </Link>
+                      {collectionDocumentsResponse.response.map((doc) => (
+                        <Dropdown.Item
+                          onClick={() => {
+                            changeDocument(doc.id);
+                          }}
+                          key={doc.id}
+                        >
+                          <span
+                            className={tw(
+                              doc.id === previewDocumentId ? "font-bold" : "",
+                            )}
+                          >
+                            {doc.name}
+                          </span>
+                        </Dropdown.Item>
+                      ))}
+                    </Dropdown>
                   </div>
-                  <PDFPage
-                    pageNumber={pageNumber}
-                    loading={renderLoadingSpinner}
-                  />
-                  <div
-                    className={tw(
-                      "flex flex-row items-center sticky bottom-0 p-2 bg-gray-50 items-center z-50 border-t border-black/10",
+                  <Link
+                    href={documentCollectionDocumentApiPath(
+                      documentCollectionId,
+                      Id.from(previewDocument.id),
                     )}
+                    target="_blank"
+                    className={tw("mr-4")}
+                    aria-label="open document in new tab"
                   >
-                    <div className={tw("flex-none")}>
-                      <Button disabled={pageNumber <= 1} onClick={previousPage}>
-                        Previous
-                      </Button>
-                    </div>
-                    <div className={tw("grow text-center")}>
-                      Page {pageNumber || (numPages ? 1 : "--")} of{" "}
-                      {numPages || "--"}
-                    </div>
-                    <div className={tw("flex-none")}>
-                      <Button
-                        disabled={pageNumber >= (numPages ?? 0)}
-                        onClick={nextPage}
-                      >
-                        Next
-                      </Button>
-                    </div>
-                  </div>
-                </PDFDocument>
+                    <HiArrowTopRightOnSquare />
+                  </Link>
+                </div>
+                <div className={tw("grow overflow-scroll")}>
+                  <Viewer
+                    fileUrl={documentCollectionDocumentApiPath(
+                      documentCollectionId,
+                      Id.from(previewDocument.id),
+                    )}
+                  />
+                </div>
               </div>
-            </div>
+            </Worker>
           </div>
           <div className={tw("w-1/2")}>
             <Chat chat={chat} chatMessages={chatMessages} documents={collectionDocumentsResponse.response} />
