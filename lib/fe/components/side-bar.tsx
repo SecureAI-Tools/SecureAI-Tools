6e268b045f3e9e30d9868b164fb04f60a3296d65
Jay Nakrani <dhananjaynakrani@gmail.com>
2023-12-04 20:58:47 -0500

    Move navigation options to cog icon on bottom left.

--- lib/fe/components/side-bar.tsx
+++ lib/fe/components/side-bar.tsx
@@ -11,7 +11,6 @@ import {
 import { tw } from "twind";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import Link from "next/link";
 
 import { FrontendRoutes } from "lib/fe/routes";
 import { TokenUser } from "lib/types/core/token-user";
@@ -37,33 +36,13 @@ export function Sidebar({
   return (
     <FlowbiteSidebar className={tw("h-screen border-r", collapsed ? "" : "w-80")} collapsed={collapsed}>
       <FlowbiteSidebar.Logo
-        href="#"
+        href={FrontendRoutes.APP_HOME}
         img="/logo.png"
         imgAlt="SecureAI Tools logo"
         className={tw("mt-8")}
         onClick={(e) => { e.preventDefault() }}
       >
-        <Dropdown
-          label={
-            <>SecureAI Tools</>
-          }
-          className={tw("z-50")}
-          placement="bottom"
-          inline
-        >
-          <Dropdown.Item
-            as="a"
-            href={FrontendRoutes.getOrgSettingsRoute(orgSlug)}
-          >
-            Organization settings
-          </Dropdown.Item>
-          <Dropdown.Item
-            as="a"
-            href={FrontendRoutes.getOrgUsersRoute(orgSlug)}
-          >
-            Organization users
-          </Dropdown.Item>
-        </Dropdown>
+        SecureAI Tools
       </FlowbiteSidebar.Logo>
       <FlowbiteSidebar.Items>
         <FlowbiteSidebar.ItemGroup>
@@ -85,7 +64,7 @@ export function Sidebar({
           </FlowbiteSidebar.Item>
         </FlowbiteSidebar.ItemGroup>
         <FlowbiteSidebar.ItemGroup
-          className={tw("absolute bottom-0 pb-8 border-t-0")}
+          className={tw("absolute bottom-0 mb-12 border-t-0")}
         >
           <FlowbiteSidebar.Item
             href="#"
@@ -108,12 +87,41 @@ export function Sidebar({
               <div className={tw("font-medium dark:text-white overflow-clip")}>
                 <div className={tw("flex flex-row items-center")}>
                   {user?.firstName} {user?.lastName}
-                  <Link
-                    href={`${FrontendRoutes.USER_SETTINGS}?src=side-bar`}
-                    className={tw("ml-1")}
+                  <Dropdown
+                    label=""
+                    dismissOnClick={false}
+                    renderTrigger={() => (
+                      <HiOutlineCog className={tw("ml-1 h-4 w-4 cursor-pointer")} />
+                    )}
+                    className={tw("z-50")}
+                    placement="top"
                   >
-                    <HiOutlineCog className={tw("h-4 w-4")} />
-                  </Link>
+                    <Dropdown.Item
+                      as="a"
+                      href={FrontendRoutes.getOrgSettingsRoute(orgSlug)}
+                    >
+                      Organization settings
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      as="a"
+                      href={FrontendRoutes.getOrgUsersRoute(orgSlug)}
+                    >
+                      Organization users
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      as="a"
+                      href={FrontendRoutes.USER_SETTINGS}
+                    >
+                      User settings
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item
+                      as="a"
+                      href={FrontendRoutes.LOG_OUT}
+                    >
+                      Log out
+                    </Dropdown.Item>
+                  </Dropdown>
                 </div>
                 <div className={tw("text-xs text-gray-500 dark:text-gray-400")}>
                   {clip(user?.email ?? "", 24)}
