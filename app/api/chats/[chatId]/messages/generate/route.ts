6e4f6f3c1f6b0948ab3202866c7fb0cefc3c0688
qchroma <qchroma>
2023-12-06 00:27:33 -0500

    Make chat with doc working

--- app/api/chats/[chatId]/messages/generate/route.ts
+++ app/api/chats/[chatId]/messages/generate/route.ts
@@ -1,7 +1,7 @@
 import { StreamingTextResponse, LangChainStream, Message } from "ai";
 import { ChatOllama } from "langchain/chat_models/ollama";
 import { AIMessage, HumanMessage } from "langchain/schema";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 import { ChatMessageService } from "lib/api/services/chat-message-service";
 import { ChatMessageRole } from "lib/types/core/chat-message-role";
@@ -13,16 +13,15 @@ import { ChatResponse } from "lib/types/api/chat.response";
 import { ChatService } from "lib/api/services/chat-service";
 import { NextResponseErrors } from "lib/api/core/utils";
 import { OllamaEmbeddings } from "langchain/embeddings/ollama";
-import { ConversationalRetrievalQAChain } from "langchain/chains"
+import { LLMChain } from "langchain/chains"
 import { Chroma } from "langchain/vectorstores/chroma"
 import { DocumentCollectionService } from "lib/api/services/document-collection-service";
 import { DocumentCollectionResponse } from "lib/types/api/document-collection.response";
 import { ChatType } from "lib/types/core/chat-type";
 import { Chat } from "@prisma/client";
-import { RunnableSequence } from "langchain/schema/runnable";
-import { formatDocumentsAsString } from "langchain/util/document";
 import { PromptTemplate } from "langchain/prompts";
 import { StringOutputParser } from "langchain/schema/output_parser";
+import { Document } from "langchain/dist/document";
 
 const chatMessageService = new ChatMessageService();
 const documentCollectionService = new DocumentCollectionService();
@@ -61,7 +60,7 @@ export async function POST(
   }
 
   if (chat.type == ChatType.CHAT_WITH_DOCS) {
-    return handleGenerateChatWithDocsConversationalRetrievalQAChain(chat, chatMessagesRequest);
+    return handleGenerateChatWithDocs(chat, chatMessagesRequest);
   } else {
     return handleGenerateChatWithAI(chat, chatMessagesRequest);
   }
@@ -106,12 +105,8 @@ async function handleGenerateChatWithAI(chat: Chat, chatMessagesRequest: ChatMes
   return new StreamingTextResponse(stream);
 }
 
-// Caveat: This does not generate the question based on chat history. It takes the last message as question and answers based on it!
-// From https://js.langchain.com/docs/modules/chains/popular/chat_vector_db/#streaming
-// 
-// To generate the question based on chat-history, we need to either use ConversationalRetrievalQAChain or do it manually as shown in example linked below
-// https://js.langchain.com/docs/modules/chains/popular/chat_vector_db/#built-in-memory
-async function handleGenerateChatWithDocsRunnableSequence(chat: Chat, chatMessagesRequest: ChatMessagesRequest) {
+
+async function handleGenerateChatWithDocs(chat: Chat, chatMessagesRequest: ChatMessagesRequest) {
   if (!chat.documentCollectionId) {
     return NextResponseErrors.notFound();
   }
@@ -138,107 +133,35 @@ async function handleGenerateChatWithDocsRunnableSequence(chat: Chat, chatMessag
   const questionPrompt = PromptTemplate.fromTemplate(
     `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
   ----------
-  CONTEXT: {context}
+  CONTEXT:
+
+  {context}
   ----------
-  CHAT HISTORY: {chatHistory}
+  CHAT HISTORY:
+
+  {chatHistory}
   ----------
-  QUESTION: {question}
+  QUESTION:
+
+  {question}
   ----------
   Helpful Answer:`
   );
 
-  // Creates basic pipe from one to the next!
-  const chain = RunnableSequence.from([
-    {
-      question: (input: { question: string; chatHistory?: string }) =>
-        input.question,
-      chatHistory: (input: { question: string; chatHistory?: string }) =>
-        input.chatHistory ?? "",
-      context: async (input: { question: string; chatHistory?: string }) => {
-        const relevantDocs = await vectorDb.asRetriever().getRelevantDocuments(input.question);
-        const serialized = formatDocumentsAsString(relevantDocs);
-        return serialized;
-      },
-    },
-    questionPrompt,
-    llm,
-    new StringOutputParser(),
-  ]);
-
-  const { messages } = chatMessagesRequest;
-
-  const query = messages.pop();
-  if (!query) {
-    return NextResponseErrors.badRequest();
-  }
-
-  const chatHistory = serializeChatHistory(messages);
-
-  const stream = await chain.stream({ question: query.content, chatHistory: chatHistory });
+  const queryRewritingPrompt = PromptTemplate.fromTemplate(
+    `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+  ----------
+  CHAT HISTORY: 
   
-  return new StreamingTextResponse(stream);
-}
-
-/*
-Uses ConversationalRetrievalQAChain
-
-BUG: This streams the generated question as well!
+  {chatHistory}
+  ----------
+  FOLLOWUP QUESTION: 
   
-ConversationalRetrievalQAChain does three things:
-  1. Generates question: If there is chat history, then based on chat history and latest question, it generates a question by asking LLM to do it.
-  2. Document Retrieval: Then it retrieves document (chunks) from vector db matching the question from step 1.
-  3. Generate answer: Finally it passes generated question, and relevant documents from previous steps into LLM to generate answer.
-
-
-The problem is that `handlers` get called for question generated in step-1 above, and so generated question gets streamed to UI!
-
-Option1:
-1. Find a way not to stream generated question back -- how do we even stop it?
-2. Just manually create a runnable sequence for all three steps!
-   Example: https://js.langchain.com/docs/modules/chains/popular/chat_vector_db/#built-in-memory
-
-*/
-async function handleGenerateChatWithDocsConversationalRetrievalQAChain(chat: Chat, chatMessagesRequest: ChatMessagesRequest) {
-  if (!chat.documentCollectionId) {
-    return NextResponseErrors.notFound();
-  }
-
-  const documentCollection = await documentCollectionService.get(Id.from<DocumentCollectionResponse>(chat.documentCollectionId));
-  if (!documentCollection) {
-    return NextResponseErrors.notFound();
-  }
-
-  const ollamaEmbeddings = new OllamaEmbeddings({
-    baseUrl: process.env.INFERENCE_SERVER,
-    model: documentCollection.model,
-  });
-
-  const vectorDb = await Chroma.fromExistingCollection(ollamaEmbeddings, {
-    collectionName: documentCollection.internalName
-  });
-
-  const llm = new ChatOllama({
-    baseUrl: process.env.INFERENCE_SERVER!,
-    model: chat.model,
-  });
-
-  const questionGeneratorLLM = new ChatOllama({
-    baseUrl: process.env.INFERENCE_SERVER!,
-    model: chat.model,
-  });
-
-  const chain = ConversationalRetrievalQAChain.fromLLM(
-    llm,
-    vectorDb.asRetriever(),
-    {
-      // DO NOT SUBMIT
-      verbose: true,
-      questionGeneratorChainOptions: {
-        llm: questionGeneratorLLM,
-      },
-    }
+  {question}
+  ----------
+  Standalone question:`
   );
-
+  
   const { messages } = chatMessagesRequest;
 
   const query = messages.pop();
@@ -246,14 +169,38 @@ async function handleGenerateChatWithDocsConversationalRetrievalQAChain(chat: Ch
     return NextResponseErrors.badRequest();
   }
 
+  console.log("original query: ", query.content);
   const chatHistory = serializeChatHistory(messages);
 
-          
-  const { stream, handlers } = LangChainStream();
-  chain
+  const queryRewritingChain = new LLMChain({
+    llm: llm,
+    prompt: queryRewritingPrompt,
+  });
+
+  const { text: rewrittenQuery } = await queryRewritingChain
     .call({
         question: query.content,
-        chat_history: chatHistory
+        chatHistory: chatHistory,
+      }
+    );
+
+  console.log("re-written query: ", rewrittenQuery);
+
+  const sources = await vectorDb.similaritySearch(String(rewrittenQuery), 2);
+
+  console.log("sources: ", sources);
+  
+  const questionChain = new LLMChain({
+    llm: llm,
+    prompt: questionPrompt,
+  });
+
+  const { stream, handlers } = LangChainStream();
+  questionChain
+    .call({
+        question: query.content,
+        chatHistory: chatHistory,
+        context: serializeSources(sources),
       },
       [handlers],
     )
@@ -274,4 +221,13 @@ const serializeChatHistory = (messages: Pick<Message, "role" | "content">[]): st
       }
     })
     .join("\n\n");
+}
+
+const serializeSources = (sources: Document[]): string => {
+  var i = 0;
+  return sources
+  .map((source) => {
+    i = i + 1;
+    return "Source: ${i}\n" + source.pageContent;
+  }).join("\n\n");
 }
