c4b7338f0a13b2848dbb213bb0fb54fd079eb0c0
qchroma <qchroma>
2023-12-13 01:15:22 -0500

    Set up message broker and receive message.

--- apps/task-master/src/main.ts
+++ apps/task-master/src/main.ts
@@ -1,5 +1,6 @@
 import { prismaClient } from "@repo/database";
 import { getLogger } from "@repo/core";
+import amqp, { Message } from "amqplib/callback_api";
 
 const logger = getLogger("task-master");
 
@@ -9,6 +10,36 @@ async function main() {
   const chats = await prismaClient.chat.findMany()
   logger.info(`found ${chats.length} chats`);
   logger.info("chats ", chats);
+
+  const rabbit_host: string = "amqp://localhost";
+  const rabbit_queue_name: string = "test-queue";
+
+  amqp.connect(rabbit_host, (err: any, conn) => {
+    if (err) {
+      logger.error("Error creating connection ", err);
+      throw err;
+    }
+  
+    conn.createChannel((err, channel) => {
+      if (err) {
+        logger.error("Error creating channel ", err);
+        throw err;
+      }
+  
+      logger.info("consumer connected");
+      channel.assertQueue(rabbit_queue_name, { durable: true });
+      channel.consume(
+        rabbit_queue_name,
+        async (data: Message | null) => {
+          if (data) {
+            const msg = JSON.parse(data.content.toString());
+            logger.info("Received message: ", msg);
+          }
+        },
+        { noAck: true }
+      );
+    });
+  });
 }
 
 main();
