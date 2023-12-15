290b89ebaf06d9b8508dc8cac9c738fe710b33ef
Jay Nakrani <dhananjaynakrani@gmail.com>
2023-12-15 13:13:05 -0500

    Get the indexing service working in online and offline modes.

--- apps/task-master/src/main.ts
+++ apps/task-master/src/main.ts
@@ -1,9 +1,11 @@
-import { prismaClient } from "@repo/database";
 import { getAMQPChannel } from "@repo/core/src/amqp-client";
 import { getLogger } from "@repo/core/src/logger";
 import { IndexingQueueMessage } from "@repo/core/src/types/indexing-queue-message";
+import { IndexingService } from "@repo/core/src/services/indexing-service";
+import { Id } from "@repo/core/src/types/id";
 
 const logger = getLogger("task-master");
+const indexingService = new IndexingService();
 
 async function main() {
   logger.info("Starting task-master...");
@@ -20,19 +22,19 @@ async function main() {
   const channel = await getAMQPChannel(amqpServerUrl);
   await channel.assertQueue(queueName);
 
+  // This consumes all messages at once in parallel. This can overwhelm the LLM.
+  // TODO: Optimize this so it only consumes max N messages at a time. We may need to do
+  // prefetch with ack: https://amqp-node.github.io/amqplib/channel_api.html#channel_prefetch
   channel.consume(
     queueName,
     async (data) => {
       if (data) {
         const msg = JSON.parse(data.content.toString()) as IndexingQueueMessage;
         logger.info("Received message: ", msg);
-        const document = await prismaClient.document.findUnique({
-          where: {
-            id: msg.documentId,
-          }
-        });
-
-        logger.info("document = ", document);
+        const asyncGenerator = indexingService.index(Id.from(msg.documentId));
+        for await (const chunk of asyncGenerator) {
+          logger.info(`[doc = ${msg.documentId}] chunk`, chunk);
+        }
       }
     },
     { noAck: true }
--- packages/core/src/services/indexing-service.ts
+++ packages/core/src/services/indexing-service.ts
@@ -1,3 +1,8 @@
+import { Document as LangchainDocument } from "langchain/dist/document";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { ChromaClient } from "chromadb";
+
 import { DocumentResponse } from "../types/document.response";
 import { Id } from "../types/id";
 import { getLogger } from "../logger";
@@ -9,37 +14,33 @@ import { DocumentIndexingStatus } from "../types/document-indexing-status";
 import { DocumentService } from "./document-service";
 import { DocumentCollectionService } from "./document-collection-service";
 
-import { Document as LangchainDocument } from "langchain/dist/document";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-
-import { ChromaClient } from "chromadb";
 import { DocumentCollectionResponse } from "../types/document-collection.response";
 import { StreamChunkResponse } from "../types/stream-chunk.response";
-
-const documentService = new DocumentService();
-const documentCollectionService = new DocumentCollectionService;
-const objectStorageService = new LocalObjectStorageService();
-const modelProviderService = new ModelProviderService();
-const chromaClient = new ChromaClient({
-  path: removeTrailingSlash(process.env.VECTOR_DB_SERVER!),
-});
 
 const logger = getLogger("model-provider-service");
 
 export class IndexingService {
-  async index(documentId: Id<DocumentResponse>): Promise<any> {
-    const document = await documentService.get(documentId);
+  private documentService = new DocumentService();
+  private documentCollectionService = new DocumentCollectionService;
+  private objectStorageService = new LocalObjectStorageService();
+  private modelProviderService = new ModelProviderService();
+  private chromaClient = new ChromaClient({
+    path: removeTrailingSlash(process.env.VECTOR_DB_SERVER!),
+  });
+
+  // Indexes given document and yields statuses that can be streamed or logged.
+  async* index(documentId: Id<DocumentResponse>): AsyncGenerator<StreamChunkResponse> {
+    const document = await this.documentService.get(documentId);
 
     if (!document) {
       throw new Error(`Document does not exist: ${documentId.toString()}`);
     }
 
-    const documentCollection = await documentCollectionService.get(Id.from<DocumentCollectionResponse>(document.collectionId));
+    const documentCollection = await this.documentCollectionService.get(Id.from<DocumentCollectionResponse>(document.collectionId));
     if (!documentCollection) {
       throw new Error(`Document collection does not exist: ${document.collectionId.toString()}`);
     }
-    
+
     const textSplitter = new RecursiveCharacterTextSplitter({
       chunkSize: process.env.DOCS_INDEXING_CHUNK_SIZE
         ? parseInt(process.env.DOCS_INDEXING_CHUNK_SIZE)
@@ -49,7 +50,7 @@ export class IndexingService {
         : 200,
     });
 
-    const fileBuffer = await objectStorageService.get(document.objectKey);
+    const fileBuffer = await this.objectStorageService.get(document.objectKey);
     var langchainDocuments: LangchainDocument[] = [];
     var loader;
     var docs;
@@ -64,87 +65,54 @@ export class IndexingService {
     }
 
     const embeddingModel =
-      modelProviderService.getEmbeddingModel(documentCollection);
-
-    const encoder = new TextEncoder();
-
-    const encodeChunk = (chunk: StreamChunkResponse): Uint8Array => {
-      const serializedChunk = JSON.stringify(chunk);
-      return encoder.encode(`${serializedChunk}\n`);
-    };
+      this.modelProviderService.getEmbeddingModel(documentCollection);
 
     // Index document and yield stream-chunks
-    const indexDocument = async function* () {
-      yield encodeChunk({ status: "Splitting documents into chunks" });
+    yield { status: "Splitting documents into chunks" };
 
-      const documentTextChunks: string[] = langchainDocuments.map(
-        (document) => document.pageContent,
-      );
+    const documentTextChunks: string[] = langchainDocuments.map(
+      (document) => document.pageContent,
+    );
 
-      yield encodeChunk({
-        status: `Processing ${documentTextChunks.length} chunks`,
+    yield { status: `Processing ${documentTextChunks.length} chunks` };
+    let embeddings: number[][] = [];
+    for (let i = 0; i < documentTextChunks.length; i++) {
+      yield { status: `Processing chunk ${i + 1} of ${documentTextChunks.length}` };
+      const documentTextChunk = documentTextChunks[i];
+      const chunkEmbedding = await embeddingModel.embedDocuments([
+        documentTextChunk!,
+      ]);
+      embeddings.push(chunkEmbedding[0]!);
+    }
+
+    const collection = await this.chromaClient.getOrCreateCollection({
+      name: documentCollection.internalName,
+    });
+
+    const addResponse = await collection.add({
+      ids: embeddings.map((_, i) => toDocumentChunkId(document.id, i)),
+      embeddings: embeddings,
+      documents: documentTextChunks,
+      metadatas: langchainDocuments.map((langchainDocument, i) =>
+        toDocumentChunkMetadata(document.id, i, langchainDocument),
+      ),
+    });
+
+    if (!isEmpty(addResponse.error)) {
+      logger.error("could not add document to vector db", {
+        documentId: document.id,
+        documentCollectionId: document.collectionId,
+        error: addResponse.error,
       });
-      let embeddings: number[][] = [];
-      for (let i = 0; i < documentTextChunks.length; i++) {
-        yield encodeChunk({
-          status: `Processing chunk ${i + 1} of ${documentTextChunks.length}`,
-        });
-        const documentTextChunk = documentTextChunks[i];
-        const chunkEmbedding = await embeddingModel.embedDocuments([
-          documentTextChunk!,
-        ]);
-        embeddings.push(chunkEmbedding[0]!);
-      }
+      yield { error: "something went wrong when indexing the doc" };
+      return;
+    }
 
-      const collection = await chromaClient.getOrCreateCollection({
-        name: documentCollection.internalName,
-      });
-
-      const addResponse = await collection.add({
-        ids: embeddings.map((_, i) => toDocumentChunkId(document.id, i)),
-        embeddings: embeddings,
-        documents: documentTextChunks,
-        metadatas: langchainDocuments.map((langchainDocument, i) =>
-          toDocumentChunkMetadata(document.id, i, langchainDocument),
-        ),
-      });
-
-      if (!isEmpty(addResponse.error)) {
-        logger.error("could not add document to vector db", {
-          documentId: document.id,
-          documentCollectionId: document.collectionId,
-          error: addResponse.error,
-        });
-        yield encodeChunk({
-          error: "something went wrong when indexing the doc",
-        });
-        return;
-      }
-
-      await documentService.update(documentId, {
-        indexingStatus: DocumentIndexingStatus.INDEXED,
-      });
-      yield encodeChunk({ status: "Document processed successfully" });
-    };
-
-    const stream = iteratorToStream(indexDocument());
-
-    return new Response(stream);
+    await this.documentService.update(documentId, {
+      indexingStatus: DocumentIndexingStatus.INDEXED,
+    });
+    yield { status: "Document processed successfully" };
   }
-}
-
-function iteratorToStream(iterator: any) {
-  return new ReadableStream({
-    async pull(controller) {
-      const { value, done } = await iterator.next();
-
-      if (done) {
-        controller.close();
-      } else {
-        controller.enqueue(value);
-      }
-    },
-  });
 }
 
 function toDocumentChunkMetadata(
